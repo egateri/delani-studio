554235da01522fbbb4e18635b6ada2f45d21ddb0
Eliud Gateri <egateri@gmail.com>
2023-04-02 05:38:27 +0300

    jQuery function to get form inputs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 
 $(document).ready(() =>{
     
-
-$("p").mouseenter(event => {
-       // $("p").text("mouseenter event has occurred");
-
-         console.log(" Mouse has entered!");
+  console.log(" document is now ready!");
+$("#myform").submit(event => {   
+  event.preventDefault();
+         console.log(" form submitted!");
+         console.log(event);
+         let name =$("#name").val();
+         let email =$("#email").val();
+         let message =$("textarea").val();
+         console.log(name );
+         console.log(email);
+         console.log(message);
+        alert(`Hello ${name} for sending as message ${message}. We truly appreciate!`);
+        $("#test").text(`Hello ${name} for sending as message ${message}. We truly appreciate!`);
     });
 });
